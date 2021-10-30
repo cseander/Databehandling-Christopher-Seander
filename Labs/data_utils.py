0d9e2515a9c1633086e1a7f265d7a5c13126f405
Christopher Seander <christopher@seander.se>
2021-10-30 22:07:42 +0200

    Part 3 of lab 1 done

--- Labs/data_utils.py
+++ Labs/data_utils.py
@@ -7,3 +7,6 @@ def px_line(data, x, y, title, labels, save_location) -> None:
     fig = px.line(data, x=x, y=y, title=title, labels=labels)
     fig.write_html(save_location)
     fig.show()
+
+def combine_year_week_numer(df) -> pd.Series:
+    return df["år"].astype(str) + "v" + df["veckonummer"].astype(str)
