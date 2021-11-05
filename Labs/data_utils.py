da26cc2657e113c739ebe4fa70762696bdac605c
Christopher Seander <christopher@seander.se>
2021-11-05 18:39:19 +0100

    finished up part 4 of the lab

--- Labs/data_utils.py
+++ Labs/data_utils.py
@@ -10,3 +10,9 @@ def px_line(data, x, y, title, labels, save_location) -> None:
 
 def combine_year_week_numer(df) -> pd.Series:
     return df["år"].astype(str) + "v" + df["veckonummer"].astype(str)
+
+def px_multiple_line(data, plot_title, legend_title, yaxis, xaxis, save_location) -> None:
+    fig = px.line(data, title=plot_title)
+    fig.update_layout(legend_title=legend_title, yaxis_title=yaxis, xaxis_title=xaxis)
+    fig.write_html(save_location)
+    fig.show()
